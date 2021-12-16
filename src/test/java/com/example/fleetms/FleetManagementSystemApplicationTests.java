de918a48e86b5e9511b7368fa0d294f6ed2510c5
Zinhle van der Westhuizen <zinhlevanderwesthuizen@gmail.com>
2021-12-16 11:23:15 +0200

    my project

---
+++ src/test/java/com/example/fleetms/FleetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fleetms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
